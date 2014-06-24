df21129565c330ca80261a1d5d7701ca79d8e2fb
Elliott B. Edwards <elliott.b.edwards@gmail.com>
2014-06-24 15:36:11 -0700

    Fixed compile error

--- src/Beard/Templates.cs
+++ src/Beard/Templates.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mona;
 
 namespace Beard
 {
     public static class Templates
     {
-        public static Compile(string uri)
+        static Templates()
         {
+            
+        }
 
+
+
+        public static void Compile(Stream input, Stream output)
+        {
+            input.CopyTo(output);
         }
     }
 }
